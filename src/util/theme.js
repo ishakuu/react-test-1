60a1277bfc74c7f2e362e8ba6c9456651ca36170
ishakuu <34801480+ishakuu@users.noreply.github.com>
2020-12-11 18:25:36 +0100

    gg

---
+++ src/util/theme.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+export default {
+    palette: {
+      primary: {
+        light: '#8f73ef',
+        main: '#7451eb',
+        dark: '#5138a4',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#f1ad6d',
+        main: '#EE9949',
+        dark: '#a66b33',
+        contrastText: '#fff',
+      },
+      error:{
+        main: '#f25f5c',
+  
+      },
+      
+      
+    },
+    spreadthis: {
+      typography: {
+        useNextVariants: true
+      },
+      Card: {
+        textAlign: "center",
+        marginTop: 35,
+        marginBottom : 50
+    },
+    title:{
+        marginTop: 20
+    },
+    form: {
+        textAlign: "center",
+        margin : 20,
+    },
+    button: {
+        margin: 15,
+        position : 'relative'
+    },
+    textField: {
+        marginTop: 20,
+        
+  
+    },
+  
+    errField: {
+        margin: 20,
+        fontSize: "1.2em",
+      fontWeight: "200",
+      backgroundColor: "#fcebeb"
+    },
+    spiner: { 
+      position: 'absolute'
+    },
+    link: {
+        textAlign: "left",
+        marginTop: 40
+    },
+  
+    }
+    
+      
+  }
